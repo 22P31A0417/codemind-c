c5a20c85d022c6931a1a7ca2dfdaff617f4a5ab2
22P31A0417 <119719064+22P31A0417@users.noreply.github.com>
2022-12-03 11:13:04 +0530

    #include<stdio.h> int bridge(int x,int y,int z) {     int r;     r=(z-y)/x;     return r; } int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     int a=bridge(x,y,z);     printf("%d",a); }

--- Can_cross_the_bridge.c
+++ Can_cross_the_bridge.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+int bridge(int x,int y,int z)
+{
+    int r;
+    r=(z-y)/x;
+    return r;
+}
 int main()
 {
-    int x,y,z,res;
+    int x,y,z;
     scanf("%d%d%d",&x,&y,&z);
-    res=(z-y)/x;
-    printf("%d",res);
+    int a=bridge(x,y,z);
+    printf("%d",a);
 }
